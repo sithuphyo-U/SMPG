d5c0c00afff3e6917d26ce6ff03546331a4c4160
sithu <sithuphyo.ny@gmail.com>
2025-05-12 14:46:45 +0630

    commit

---
+++ DIS.DataAccess/Entity/Role.cs
@@ -0,0 +1,18 @@
+﻿using DIS.Infrastructure.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIS.DataAccess.Entity
+{
+    [Table("role")]
+    public class Role : BaseEntity
+    {
+        public string? name { get; set; }
+        public string? description { get; set; }
+       
+    }
+}
---
+++ DIS.DataAccess/Repositories/RoleRepository.cs
@@ -0,0 +1,22 @@
+﻿using DIS.DataAccess.Entity;
+using DIS.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DIS.DataAccess.Repositories
+{
+    public class RoleRepository : ReadWriteRepositoryBase<Role>, IRoleRepository
+    {
+        public RoleRepository(IDbContext context) : base(context)
+        {
+        }
+        public Role? FindByName(string name)
+        {
+            return CustomQuery().Where(x => x.name == name && x.deleted == false).FirstOrDefault();
+        }
+    }
+}
