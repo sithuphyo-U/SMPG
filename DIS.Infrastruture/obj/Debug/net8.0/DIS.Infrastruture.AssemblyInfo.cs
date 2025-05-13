686c273da4bddb5c6128cf364bdf7071de3643b2
NwayZinWin01 <nwayzinwin.ny@gmail.com>
2025-05-13 14:08:58 +0630

    commit

--- DIS.Infrastruture/obj/Debug/net8.0/DIS.Infrastruture.AssemblyInfo.cs
+++ DIS.Infrastruture/obj/Debug/net8.0/DIS.Infrastruture.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DIS.Infrastruture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4220abfe85012eb2f1b4e23712390d4b733e5538")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+373b3136e8278df235c31632ef47034b72a80347")]
 [assembly: System.Reflection.AssemblyProductAttribute("DIS.Infrastruture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DIS.Infrastruture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ DIS.web/ViewModels/ProgramCodeViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace DIS.Web.ViewModels
+{
+    public class ProgramCodeViewModel
+    {
+        public int? id {  get; set; }
+        public string? program_name { get; set; }
+        public string? program_code { get; set; }
+        public int? parent_id { get; set; }
+        public string? url { get; set; }
+        public string? icon { get; set; }
+        public string? permission { get; set; }
+        public List<PermissionViewModel>? permissions { get; set; }
+        public List<object> children { get; set; }
+        public ProgramCodeViewModel()
+        {
+            permissions = new List<PermissionViewModel>();
+            children = new List<object>();
+        }
+
+    }
+}
