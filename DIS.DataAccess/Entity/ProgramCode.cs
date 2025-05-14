ff18806347505f2659d2dae54640e68d1fdf7d87
ZuZuZaw <zuzuzaw.ny@gmail.com>
2025-05-14 16:21:11 +0630

    commit

--- DIS.DataAccess/Entity/ProgramCode.cs
+++ DIS.DataAccess/Entity/ProgramCode.cs
@@ -1,12 +1,14 @@
 ﻿using DIS.Infrastructure.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DIS.DataAccess.Entity
 {
+    [Table("program_code")]
     public class ProgramCode :BaseEntity
     {
 public string program_name { get; set; }
