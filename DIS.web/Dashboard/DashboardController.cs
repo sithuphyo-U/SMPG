20e63ae3656dcdff7e94f3ed6ae70abb78695b6c
NwayZinWin01 <nwayzinwin.ny@gmail.com>
2025-05-26 11:03:58 +0630

    commit

--- DIS.web/Dashboard/DashboardController.cs
+++ DIS.web/Dashboard/DashboardController.cs
@@ -1,28 +1,28 @@
-﻿using DIS.DataAccess.Interfaces;
-using DIS.Web.Controllers.Common;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+﻿//using DIS.DataAccess.Interfaces;
+//using DIS.Web.Controllers.Common;
+//using Microsoft.AspNetCore.Mvc;
+//using Newtonsoft.Json;
 
-namespace DIS.Web.Dashboard
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DashboardController : BaseController
-    {   
-        IDIInfoByYearRangeDashboardRepository _yearRangeRepository;
+//namespace DIS.Web.Dashboard
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class DashboardController : BaseController
+//    {   
+//        IDIInfoByYearRangeDashboardRepository _yearRangeRepository;
         
-        public DashboardController(IDIInfoByYearRangeDashboardRepository yearRangeRepository) : base(typeof(DashboardController))
-        {
-            _yearRangeRepository = yearRangeRepository;
-        }
+//        public DashboardController(IDIInfoByYearRangeDashboardRepository yearRangeRepository) : base(typeof(DashboardController))
+//        {
+//            _yearRangeRepository = yearRangeRepository;
+//        }
 
-        [HttpGet]
-        [Route("GetDisasterInfoByYearRange/")]
-        public IActionResult GetDisasterInfoByYearRange(int year)
-        {
-            var reports = _yearRangeRepository.GetInfoByYearRange(year);
-            Console.WriteLine(JsonConvert.SerializeObject(reports));
-            return Json(reports);
-        }
-    }
-}
+//        [HttpGet]
+//        [Route("GetDisasterInfoByYearRange/")]
+//        public IActionResult GetDisasterInfoByYearRange(int year)
+//        {
+//            var reports = _yearRangeRepository.GetInfoByYearRange(year);
+//            Console.WriteLine(JsonConvert.SerializeObject(reports));
+//            return Json(reports);
+//        }
+//    }
+//}
