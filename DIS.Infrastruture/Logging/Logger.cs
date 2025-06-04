3341ac58ea6341134aaf353ab024a4c6155120b9
sithu <sithuphyo.ny@gmail.com>
2025-06-04 20:52:14 +0630

    commit

--- DIS.Infrastruture/Logging/Logger.cs
+++ DIS.Infrastruture/Logging/Logger.cs
@@ -14,7 +14,7 @@ namespace DIS.Infrastructure.Logging
         {
             this.log4NetAdapter = LogManager.GetLogger(type);
         }
-        public void LogError(Exception ex, string message)
+        public void LogError(string message)
         {
             this.log4NetAdapter.Error(message);
         }
