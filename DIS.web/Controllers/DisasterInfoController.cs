f4034145be7cee46261be5daf5d16a02cefa6f34
ZuZuZaw <zuzuzaw.ny@gmail.com>
2025-06-05 13:29:30 +0630

    commit

--- DIS.web/Controllers/DisasterInfoController.cs
+++ DIS.web/Controllers/DisasterInfoController.cs
@@ -691,10 +691,9 @@ namespace DIS.Web.Controllers
                 {
                     if (!string.IsNullOrEmpty(searchWord))
                     {
-                        string normalizedText = page.Text.Normalize(NormalizationForm.FormKC);
-                        string normalizedSearch = searchWord.Normalize(NormalizationForm.FormKC);
-
-                        int count = Regex.Matches(normalizedText, Regex.Escape(normalizedSearch), RegexOptions.IgnoreCase).Count;
+                        string normalizedText = page.Text;
+                        int count = Regex.Matches(normalizedText, Regex.Escape(searchWord), RegexOptions.IgnoreCase).Count;
+                        //int count = Regex.Matches(normalizedText, Regex.Escape(normalizedSearch), RegexOptions.IgnoreCase).Count;
 
                         //int count = Regex.Matches(page.Text, Regex.Escape("Javascript"), RegexOptions.IgnoreCase).Count;
                         totalWordCount += count;
