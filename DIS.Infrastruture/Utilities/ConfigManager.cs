e42f49b642a52dfad9231f97b2b78b24865fb5ed
sithu <sithuphyo.ny@gmail.com>
2026-09-17 15:48:47 +0630

    Fix configuration for cloud deployment

--- DIS.Infrastruture/Utilities/ConfigManager.cs
+++ DIS.Infrastruture/Utilities/ConfigManager.cs
@@ -14,9 +14,16 @@ namespace DIS.Infrastructure.Utilities
         {
             AppSetting = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
                 .Build();
         }
+        //static ConfigManager()
+        //{
+        //    var builder = new ConfigurationBuilder()
+        //        .SetBasePath(Directory.GetCurrentDirectory())
+        //        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+        //         .Build();
+        //}
         public static string GetConnectionString()
         {
             var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
